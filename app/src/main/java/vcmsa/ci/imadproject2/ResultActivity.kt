c5847d9600acdf7553c42261a04cca5f2caa58fe
lab_services_student <st10470447@vcconnect.edu.za>
2025-05-20 13:52:14 +0200

    second commit

--- app/src/main/java/vcmsa/ci/imadproject2/ResultActivity.kt
+++ app/src/main/java/vcmsa/ci/imadproject2/ResultActivity.kt
@@ -1,7 +1,10 @@
 package vcmsa.ci.imadproject2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,6 +14,36 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
+
+        val score = intent.getIntExtra("SCORE",0) // change for all correct answers
+        val total = intent. getIntExtra("TOTAL",5) // DEFAULT TO 5
+        val correctAnswers = intent.getStringArrayListExtra("CORRECT_ANSWERS") ?: arrayListOf()
+
+
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        val feedbackTextView = findViewById<TextView>(R.id.feedbackTextView)
+        val reviewButton = findViewById<Button>(R.id.reviewButton)
+
+        resultTextView.text = "you got $score out of $total correct"
+
+        //Feedback based on performance
+        val feedback = when {
+            score == total -> "Great work! You are a BrainTeaser pro"
+            score >= total/ 2 ->"good try , keep learning"
+            else -> "Try Again Next Time"
+        }
+        feedbackTextView.text = feedback
+
+        reviewButton.setOnClickListener {
+            val message =correctAnswers.joinToString("\n. ", prefix = ".")
+
+
+            AlertDialog.Builder(this)
+                .setTitle("All Correct Answers")
+                .setMessage(message)
+                .setPositiveButton("OK", null)
+                .show()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/vcmsa/ci/imadproject2/SecondActivity.kt
+++ app/src/main/java/vcmsa/ci/imadproject2/SecondActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import vcmsa.ci.imadproject2.R
 
+
 class SecondActivity : AppCompatActivity() {
 /
 
